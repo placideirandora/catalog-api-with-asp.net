8c1a1582a9337b5e7670e3bb621cc1610ffff8ac
Placide IRANDORA <34879554+placiderapson@users.noreply.github.com>
2021-06-30 14:45:08 +0200

    Create a Catalog Item Repository With Retrieval Methods

---
+++ Repositories/InMemoryItemsRepository.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+using CatalogApi.Entities;
+using System.Collections.Generic;
+
+namespace CatalogApi.Repositories
+{
+    public class InMemoryItemsRepository
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow }
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+        }
+    }
+}
