d8b1c6c6afb40460b002f9a8de48b426a9f9ae00
Placide IRANDORA <34879554+placiderapson@users.noreply.github.com>
2021-07-01 21:47:46 +0200

    Setup MongoDb for Data Persistence

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,15 @@
+using MongoDB.Bson;
+using MongoDB.Driver;
+using CatalogApi.Settings;
 using CatalogApi.Repositories;
 using Microsoft.OpenApi.Models;
+using MongoDB.Bson.Serialization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using CatalogApi.Repositories.Interfaces;
+using MongoDB.Bson.Serialization.Serializers;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CatalogApi
@@ -21,8 +26,18 @@ namespace CatalogApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // MongoDb Serialization for Guid and DateTimeOffset
+            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+            // Services Registration
             services.AddControllers();
-            services.AddSingleton<IInMemoryItemsRepository, InMemoryItemsRepository>();
+            services.AddSingleton<IMongoClient>(serviceProvider =>
+            {
+                var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+                return new MongoClient(settings.ConnectionString);
+            });
+            services.AddSingleton<IMongoDbItemsRepository, MongoDbItemsRepository>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CatalogApi", Version = "v1" });
