614915d649c4bb59c5c9a04d22c17a64616aac6d
Placide IRANDORA <34879554+placiderapson@users.noreply.github.com>
2021-07-02 22:32:35 +0200

    Implement Health Check Endpoints for Database and Services

--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,13 @@
+using System;
+using System.Linq;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using System.Net.Mime;
+using System.Text.Json;
 using CatalogApi.Settings;
 using CatalogApi.Repositories;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Http;
 using MongoDB.Bson.Serialization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -11,6 +16,7 @@ using Microsoft.Extensions.Configuration;
 using CatalogApi.Repositories.Interfaces;
 using MongoDB.Bson.Serialization.Serializers;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace CatalogApi
 {
@@ -30,6 +36,8 @@ namespace CatalogApi
             BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
             BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
+            var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+
             // Services Registration
             services.AddControllers(options =>
             {
@@ -37,14 +45,17 @@ namespace CatalogApi
             });
             services.AddSingleton<IMongoClient>(serviceProvider =>
             {
-                var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-                return new MongoClient(settings.ConnectionString);
+                return new MongoClient(mongoDbSettings.ConnectionString);
             });
             services.AddSingleton<IMongoDbItemsRepository, MongoDbItemsRepository>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CatalogApi", Version = "v1" });
             });
+            services.AddHealthChecks()
+                .AddMongoDb(mongoDbSettings.ConnectionString, name: "mongodb",
+                timeout: TimeSpan.FromSeconds(3),
+                 tags: new[] { "ready" });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -66,7 +77,33 @@ namespace CatalogApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions
+                {
+                    Predicate = (check) => check.Tags.Contains("ready"),
+                    ResponseWriter = async (context, report) =>
+                    {
+                        var result = JsonSerializer.Serialize(new
+                        {
+                            status = report.Status.ToString(),
+                            checks = report.Entries.Select(entry => new
+                            {
+                                name = entry.Key,
+                                status = entry.Value.Status.ToString(),
+                                exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                                duration = entry.Value.Duration.ToString()
+                            })
+                        });
+
+                        context.Response.ContentType = MediaTypeNames.Application.Json;
+                        await context.Response.WriteAsync(result);
+                    }
+                });
+                endpoints.MapHealthChecks("health/live", new HealthCheckOptions
+                {
+                    Predicate = (_) => false
+                });
             });
         }
     }
 }
+  
