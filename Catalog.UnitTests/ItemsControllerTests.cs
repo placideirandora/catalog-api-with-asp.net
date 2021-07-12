3e9d1d55ae7688c5ca61a497979cb22315255d3f
Placide IRANDORA <34879554+placiderapson@users.noreply.github.com>
2021-07-12 20:35:18 +0200

    Unit Test the Second Scenario of the Get Item Endpoint for Returning an Existing Item

--- Catalog.UnitTests/ItemsControllerTests.cs
+++ Catalog.UnitTests/ItemsControllerTests.cs
@@ -1,6 +1,7 @@
 using Moq;
 using Xunit;
 using System;
+using FluentAssertions;
 using Catalog.Api.Entities;
 using System.Threading.Tasks;
 using Catalog.Api.Controllers;
@@ -12,24 +13,53 @@ namespace Catalog.UnitTests
 {
     public class ItemsControllerTests
     {
+        private readonly Random rand = new();
+        private readonly Mock<ILogger<ItemsController>> loggerStub = new();
+        private readonly Mock<IMongoDbItemsRepository> repositoryStub = new();
+
+
         [Fact]
         public async Task GetItemAsync_WithUnexistingItem_ReturnsNotFound()
         {
             // Arrange
-            var repositoryStub = new Mock<IMongoDbItemsRepository>();
             repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>())).ReturnsAsync((Item)null);
 
-            var loggerStub = new Mock<ILogger<ItemsController>>();
-
             var controller = new ItemsController(repositoryStub.Object, loggerStub.Object);
-
 
             // Act
             var result = await controller.GetItemAsync(Guid.NewGuid());
 
             // Assert
             Assert.IsType<NotFoundResult>(result.Result);
+        }
 
+        [Fact]
+        public async Task GetItemAsync_WithExistingItem_ReturnsExpectedItem()
+        {
+            // Arrange
+            var expectedItem = CreateRandomItem();
+
+            repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>())).ReturnsAsync(expectedItem);
+
+            var controller = new ItemsController(repositoryStub.Object, loggerStub.Object);
+
+            // Act
+            var result = await controller.GetItemAsync(Guid.NewGuid());
+
+            // Assert
+            result.Value.Should().BeEquivalentTo(expectedItem, options => options.ComparingByMembers<Item>());
+        }
+
+
+        private Item CreateRandomItem()
+        {
+            return new()
+            {
+                Id = Guid.NewGuid(),
+                Name = Guid.NewGuid().ToString(),
+                Price = rand.Next(1000),
+                CreatedDate = DateTimeOffset.UtcNow
+            };
         }
     }
 }
