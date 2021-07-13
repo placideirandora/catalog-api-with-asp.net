671879f2acda92bdc706c8133e37deadaebae262
Placide IRANDORA <34879554+placiderapson@users.noreply.github.com>
2021-07-13 22:59:29 +0200

    Unit Test the Update Item Endpoint for Ensuring Item Update

--- Catalog.UnitTests/ItemsControllerTests.cs
+++ Catalog.UnitTests/ItemsControllerTests.cs
@@ -89,6 +89,43 @@ namespace Catalog.UnitTests
             createdItem.CreatedDate.Should().BeCloseTo(DateTimeOffset.UtcNow, 1000);
         }
 
+        [Fact]
+        public async Task UpdateItemAsync_WithUnexistingItem_ReturnsNotFound()
+        {
+            // Arrange
+            repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>())).ReturnsAsync((Item)null);
+
+            var itemToUpdate = new UpdateItemDto() { Name = Guid.NewGuid().ToString(), Price = rand.Next(1000) };
+
+            var controller = new ItemsController(repositoryStub.Object, loggerStub.Object);
+
+            // Act
+            var result = await controller.UpdateItemAsync(Guid.NewGuid(), itemToUpdate);
+
+            // Assert
+            result.Should().BeOfType<NotFoundResult>();
+        }
+
+        [Fact]
+        public async Task UpdateItemAsync_WithExistingItem_ReturnsNoContent()
+        {
+            // Arrange
+            var existingItem = CreateRandomItem();
+
+            repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>())).ReturnsAsync(existingItem);
+
+            var itemId = existingItem.Id;
+            var itemToUpdate = new UpdateItemDto() { Name = Guid.NewGuid().ToString(), Price = existingItem.Price + 3 };
+
+            var controller = new ItemsController(repositoryStub.Object, loggerStub.Object);
+
+            // Act
+            var result = await controller.UpdateItemAsync(itemId, itemToUpdate);
+
+            // Assert
+            result.Should().BeOfType<NoContentResult>();
+        }
+
 
         private Item CreateRandomItem()
         {
